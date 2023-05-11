e816d2b2e7f12a119523f7a54e922ce5d9a4e812
Norixpy <norixmaidana@gmail.com>
2023-05-10 20:18:37 -0400

    solucion a los ejercicios de js

---
+++ js-1/hello.js
@@ -0,0 +1,10 @@
+// escribe la función hola acá
+function hola (nombre) {
+    let saludo = "hola" + nombre + "!"
+    return saludo
+}
+
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
